bb2e1b5ab9d35eae7ce0af71367bf5d002539dbe
Jesse Theisen <theiseja@gmail.com>
2020-02-19 22:37:45 -0600

    Adds lecture notes

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
+from dll_stack import Stack
+from dll_queue import Queue
 import sys
 sys.path.append('../queue_and_stack')
-from dll_queue import Queue
-from dll_stack import Stack
 
 
 class BinarySearchTree:
@@ -12,65 +12,61 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        # BST was empty
-            if self.value is None:
-                self.value = BinarySearchTree(value)
-                self.value = value
-            # insert into right subtree
-            elif value > self.value:
-                # TBC
-            # insert into left subtree:
-                
+        pass
 
     # Return True if the tree contains the value
     # False if it does not
-def contains(self, target):
-    
-    # !. base cases:
-    # target found or we hit none
-    
-    # 2. Recursive case
-    # go down left or right subtree
+    def contains(self, target):
         pass
 
     # Return the maximum value found in the tree
-def get_max(self):
-    # RIGHT as far as you can go
+    def get_max(self):
         pass
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
-def for_each(self, cb):
-    # 1. base case: left is None or Right is None
-    
-    # 2. Recursive case:
-    # go left and right 
+    def for_each(self, cb):
         pass
 
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
-def in_order_print(self, node):
+    def in_order_print(self, node):
         pass
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
-def bft_print(self, node):
+    def bft_print(self, node):
+        # set up a QUEUE [nodes to backtrack to]
+        # init with 'node'
+        
+        # while the queue is NOT empty
+            # dequeue node
+            # print node.value
+            # enqueue node.left, node.right
+            
         pass
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
-def dft_print(self, node):
+    def dft_print(self, node):
+        # set up a stack [node we need to backtrack to because we haven't visited both subtrees yet]
+        # init with root node
+        
+        # while stack NOT empty, one thing remains that must be backtracked through
+            # pop node from stacl
+            # print the node.value
+            # push node.left, node.right
         pass
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
 
     # Print Pre-order recursive DFT
-def pre_order_dft(self, node):
+    def pre_order_dft(self, node):
         pass
 
     # Print Post-order recursive DFT
-def post_order_dft(self, node):
+    def post_order_dft(self, node):
         pass
